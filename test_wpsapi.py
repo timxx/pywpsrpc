3b8523b5d93a0ac22deca820eb8e65c1e1ed0ff1
Weitian Leung <weitianleung@gmail.com>
2020-01-19 19:10:43 +0800

    mark some arguments as optional

--- test_wpsapi.py
+++ test_wpsapi.py
@@ -10,66 +10,96 @@ import wpssdk
 from wpssdk import wpsapi
 
 
+def check_call(funcName, hr, value=None):
+    if hr != 0:
+        print("call {} failed with code: {}".format(funcName, hr))
+        sys.exit(-1)
+    if value != None:
+        print("{}: {}".format(funcName, value))
+    else:
+        print("{}: <ok>".format(funcName))
+
+
+
 def test():
     qapp = QCoreApplication(sys.argv)
 
     hr, rpc = wpssdk.createWpsRpcInstance()
-    print(hr, rpc)
+    check_call("createWpsRpcInstance", hr, rpc)
 
     hr = rpc.setProcessArgs([__file__])
-    print(hr)
+    check_call("setProcessArgs", hr)
 
     wpsApp = wpssdk.getWpsApplication(rpc)
 
     hr, pid = rpc.getProcessPid()
-    print(hr, pid)
+    check_call("getProcessPid", hr, pid)
 
     hr, docs = wpsApp.get_Documents()
-    print(hr, docs)
-    print(docs.get_Count())
+    check_call("get_Documents", hr, docs)
+
+    hr, count = docs.get_Count()
+    check_call("get_Count", hr, count)
 
     hr, doc = wpsApp.get_ActiveDocument()
-    print(hr, doc)
-    print(doc.get_Name())
-    # TODO: make the argument as optional
-    hr = doc.SaveAs("test.docx",
-        wpsapi.wdFormatDocument, # FileFormat
-        False, # LockComments
-        "", # Password
-        False, # AddToRecentFiles
-        "", # WritePassword
-        False, # ReadOnlyRecommended
-        False, # EmbedTrueTypeFonts
-        False, # SaveNativePictureFormat
-        None, # SaveFormsData
-        None, # SaveAsAOCELetter
-        None, # Encoding
-        None, # InsertLineBreaks
-        None, # AllowSubstitutions
-        None, # LineEnding
-        None # AddBiDiMarks
-        )
-    doc.Close(None, None, None)
+    check_call("get_ActiveDocument", hr, doc)
 
-    hr, doc = docs.Add(None, None, None, True)
-    print(hr, doc)
-    print(doc.get_Name())
+    hr, name = doc.get_Name()
+    check_call("get_Name", hr, name)
 
-    print(wpsApp.get_Name())
-    print(wpsApp.get_Version())
-    print(wpsApp.get_Build())
+    hr = doc.SaveAs("test.docx", wpsapi.wdFormatDocument)
+    check_call("SaveAs", hr)
 
-    print(wpsApp.get_Caption())
-    wpsApp.put_Caption("test标题")
-    print(wpsApp.get_Caption())
+    hr = doc.Close()
+    check_call("Close", hr)
 
-    wpsApp.put_Visible(False)
-    print(wpsApp.get_Visible())
+    hr, doc = docs.Open("test.docx", ReadOnly=True)
+    check_call("Open", hr, doc)
 
-    wpsApp.put_Visible(True)
-    print(wpsApp.get_Visible())
+    hr, readOnly = doc.get_ReadOnly()
+    check_call("get_ReadOnly", hr, readOnly)
 
-    wpsApp.Quit(wpsapi.wdDoNotSaveChanges, None, None)
+    doc.Close()
+    check_call("Close", hr)
+
+    hr, doc = docs.Add(Visible=False)
+    check_call("Add", hr, doc)
+
+    hr, name = doc.get_Name()
+    check_call("get_Name", hr, name)
+
+    hr, name = wpsApp.get_Name()
+    check_call("get_Name", hr, name)
+
+    hr, version = wpsApp.get_Version()
+    check_call("get_Version", hr, version)
+
+    hr, build = wpsApp.get_Build()
+    check_call("get_Build", hr, build)
+
+    hr, caption = wpsApp.get_Caption()
+    check_call("get_Caption", hr, caption)
+
+    hr = wpsApp.put_Caption("test标题")
+    check_call("put_Caption", hr)
+
+    hr, caption = wpsApp.get_Caption()
+    check_call("get_Caption", hr, caption)
+
+    hr = wpsApp.put_Visible(False)
+    check_call("put_Visible", hr)
+
+    hr, visible = wpsApp.get_Visible()
+    check_call("get_Visible", hr, visible)
+
+    hr = wpsApp.put_Visible(True)
+    check_call("put_Visible", hr)
+
+    hr, visible = wpsApp.get_Visible()
+    check_call("get_Visible", hr, visible)
+
+    hr = wpsApp.Quit(wpsapi.wdDoNotSaveChanges)
+    check_call("Quit", hr)
 
     #qapp.exec()
 
