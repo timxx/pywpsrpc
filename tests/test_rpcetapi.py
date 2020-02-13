f005f0651fec1f629570f5f8e554711be9c0563a
Weitian Leung <weitianleung@gmail.com>
2020-02-13 22:34:14 +0800

    complete the rpcetapi bindings

---
+++ tests/test_rpcetapi.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../build")
+
+from pywpsrpc.rpcetapi import etapi
+from pywpsrpc import rpcetapi
+
+
+def check_call(funcName, hr, value=None):
+    if hr != 0:
+        print("call {} failed with code: {}".format(funcName, hr))
+        sys.exit(-1)
+    if value != None:
+        print("{}: {}".format(funcName, value))
+    else:
+        print("{}: <ok>".format(funcName))
+
+
+def test():
+    hr, rpc = rpcetapi.createEtRpcInstance()
+    check_call("rpcetapi.createEtRpcInstance", hr, rpc)
+
+    app = rpcetapi.getEtApplication(rpc)
+    print(app)
+
+    hr, pid = rpc.getProcessPid()
+    check_call("rpc.getProcessPid", hr, pid)
+
+    hr, workbooks = app.get_Workbooks()
+    check_call("app.get_Workbooks", hr ,workbooks)
+
+    hr, workbook = workbooks.Add()
+    check_call("workbooks.Add", hr, workbook)
+
+    hr = workbook.Close()
+    check_call("workbook.Close", hr)
+
+
+    app.Quit()
+
+
+if __name__ == "__main__":
+    test()
