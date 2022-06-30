232a34face6a1d53c8d4fafd9b1c49b43ec28281
Weitian Leung <weitianleung@gmail.com>
2022-06-30 13:35:10 +0800

    [tests] Switch to unittest

--- tests/test_rpcwpsapi.py
+++ tests/test_rpcwpsapi.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import time
+import unittest
 
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/../build")
 from pywpsrpc import rpcwpsapi
@@ -10,312 +11,317 @@ from pywpsrpc import common
 from pywpsrpc.rpcwpsapi import wpsapi
 from pywpsrpc.utils import RpcIter
 
+class TestRpcWpsApi(unittest.TestCase):
 
-def check_call(funcName, hr, value=None):
-    if hr != common.S_OK:
-        print("call {} failed with code: {}".format(funcName, hr))
-        sys.exit(-1)
-    if value != None:
-        print("{}: {}".format(funcName, value))
-    else:
-        print("{}: <ok>".format(funcName))
+    def check_call(self, funcName, hr):
+        self.assertEqual(hr, common.S_OK, funcName)
 
+    def test(self):
+        hr, rpc = rpcwpsapi.createWpsRpcInstance()
+        self.check_call("createWpsRpcInstance", hr)
 
-def test():
-    hr, rpc = rpcwpsapi.createWpsRpcInstance()
-    check_call("createWpsRpcInstance", hr, rpc)
+        hr = rpc.setProcessArgs(
+            [os.path.dirname(os.path.realpath(__file__)) + "/../pyproject.toml"])
+        self.check_call("setProcessArgs", hr)
 
-    hr = rpc.setProcessArgs([os.path.dirname(os.path.realpath(__file__)) + "/../pyproject.toml"])
-    check_call("setProcessArgs", hr)
+        hr, wpsApp = rpc.getWpsApplication()
+        self.check_call("rpc.getWpsApplication", hr)
 
-    hr, wpsApp = rpc.getWpsApplication()
-    check_call("rpc.getWpsApplication", hr, wpsApp)
+        hr, pid = rpc.getProcessPid()
+        self.check_call("getProcessPid", hr)
+        self.assertNotEqual(pid, 0, "rpc.getProcessPid")
 
-    hr, pid = rpc.getProcessPid()
-    check_call("getProcessPid", hr, pid)
+        hr, docs = wpsApp.get_Documents()
+        self.check_call("get_Documents", hr)
 
-    hr, docs = wpsApp.get_Documents()
-    check_call("get_Documents", hr, docs)
+        # FIXME: some env may fails get_Count (returns 0)
+        time.sleep(0.5)
 
-    # FIXME: some env may fails get_Count (returns 0)
-    time.sleep(0.5)
+        hr, count = docs.get_Count()
+        self.check_call("get_Count", hr)
+        self.assertEqual(count, 1)
 
-    hr, count = docs.get_Count()
-    check_call("get_Count", hr, count)
+        hr, doc = wpsApp.get_ActiveDocument()
+        self.check_call("get_ActiveDocument", hr)
 
-    hr, doc = wpsApp.get_ActiveDocument()
-    check_call("get_ActiveDocument", hr, doc)
+        hr, name = doc.get_Name()
+        self.check_call("get_Name", hr)
+        self.assertEqual(name, "pyproject.toml", "doc.get_Name")
 
-    hr, name = doc.get_Name()
-    check_call("get_Name", hr, name)
+        hr, docRange = doc.get_Content()
+        self.check_call("get_Content", hr)
 
-    hr, docRange = doc.get_Content()
-    check_call("get_Content", hr, docRange)
+        hr, start = docRange.get_Start()
+        self.check_call("get_Start", hr)
 
-    hr, start = docRange.get_Start()
-    check_call("get_Start", hr, start)
+        hr, oldEnd = docRange.get_End()
+        self.check_call("get_End", hr)
 
-    hr, end = docRange.get_End()
-    check_call("get_End", hr, end)
+        hr = docRange.InsertParagraphAfter()
+        self.check_call("InsertParagraphAfter", hr)
 
-    hr, text = docRange.get_Text()
-    check_call("get_Text", hr, text)
-    # the text uses \r as newline, LoL
-    print(text.replace("\r", "\n"))
+        hr, end = docRange.get_End()
+        self.assertEqual(end, oldEnd + 1, "InsertParagraphAfter")
 
-    hr, paras = doc.get_Paragraphs()
-    check_call("get_Paragraphs", hr, paras)
+        hr, text = docRange.get_Text()
+        self.check_call("get_Text", hr)
 
-    hr, count = paras.get_Count()
-    check_call("get_Count", hr, count)
+        hr = docRange.InsertAfter("Hello, world")
+        self.check_call("InsertAfter", hr)
 
-    hr = docRange.InsertParagraphAfter()
-    check_call("InsertParagraphAfter", hr)
+        end -= 1
+        hr = docRange.put_Start(end)
+        self.check_call("put_Start", hr)
 
-    hr = docRange.InsertAfter("Hello, world")
-    check_call("InsertAfter", hr)
+        hr = docRange.put_End(end + 12)
+        self.check_call("put_End", hr)
 
-    hr, count = paras.get_Count()
-    check_call("get_Count", hr, count)
+        hr, text = docRange.get_Text()
+        self.check_call("get_Text", hr)
+        self.assertEqual(text, "Hello, world", "docRange.get_Text")
 
-    hr, app2 = paras.get_Application()
-    check_call("paras.get_Application", hr, app2)
+        hr, paras = doc.get_Paragraphs()
+        self.check_call("get_Paragraphs", hr)
 
-    hr, build = app2.get_Build()
-    check_call("app2.get_Build", hr, build)
+        hr, count = paras.get_Count()
+        self.check_call("get_Count", hr)
 
-    hr, para = paras.get_First()
-    check_call("paras.get_First", hr, para)
+        docRange.InsertParagraphAfter()
 
-    hr, dropCap = para.get_DropCap()
-    check_call("para.get_DropCap", hr, dropCap)
+        hr, newCount = paras.get_Count()
+        self.check_call("get_Count", hr)
+        self.assertEqual(newCount, count + 1, "paras.get_Count")
 
-    hr = dropCap.put_Position(wpsapi.wdDropNormal)
-    check_call("dropCap.put_Position", hr)
+        hr, app2 = paras.get_Application()
+        self.check_call("paras.get_Application", hr)
 
-    hr, paraRange = para.get_Range()
-    check_call("para.get_Range", hr, paraRange)
+        hr, build = app2.get_Build()
+        self.check_call("app2.get_Build", hr)
 
-    hr, text = paraRange.get_Text()
-    check_call("paraRange.get_Text", hr, text)
+        hr, para = paras.get_First()
+        self.check_call("paras.get_First", hr)
 
-    hr = para.put_Alignment(wpsapi.wdAlignParagraphCenter)
-    check_call("para.put_Alignment", hr)
+        hr, dropCap = para.get_DropCap()
+        self.check_call("para.get_DropCap", hr)
 
-    hr, paraFormat = para.get_Format()
-    check_call("para.get_Format", hr, paraFormat)
+        hr = dropCap.put_Position(wpsapi.wdDropNormal)
+        self.check_call("dropCap.put_Position", hr)
 
-    hr, style = paraFormat.get_Style()
-    check_call("paraFormat.get_Style", hr, style)
+        hr, paraRange = para.get_Range()
+        self.check_call("para.get_Range", hr)
 
-    hr, borders = paraFormat.get_Borders()
-    check_call("paraFormat.get_Borders", hr, borders)
+        hr, text = paraRange.get_Text()
+        self.check_call("paraRange.get_Text", hr)
 
-    hr, count = borders.get_Count()
-    check_call("borders.get_Count", hr, count)
+        hr = para.put_Alignment(wpsapi.wdAlignParagraphCenter)
+        self.check_call("para.put_Alignment", hr)
 
-    hr = borders.put_Shadow(True)
-    check_call("borders.put_Shadow", hr)
+        hr, paraFormat = para.get_Format()
+        self.check_call("para.get_Format", hr)
 
-    hr = paraFormat.put_Borders(borders)
-    check_call("paraFormat.put_Borders", hr)
+        hr, style = paraFormat.get_Style()
+        self.check_call("paraFormat.get_Style", hr)
 
-    hr, shading = para.get_Shading()
-    check_call("para.get_Shading", hr, shading)
+        hr, borders = paraFormat.get_Borders()
+        self.check_call("paraFormat.get_Borders", hr)
 
-    hr = shading.put_BackgroundPatternColor(wpsapi.wdColorGreen)
-    check_call("shading.put_BackgroundPatternColor", hr)
+        hr, count = borders.get_Count()
+        self.check_call("borders.get_Count", hr)
 
-    hr, tabStops = para.get_TabStops()
-    check_call("para.get_TabStops", hr, tabStops)
+        hr = borders.put_Shadow(True)
+        self.check_call("borders.put_Shadow", hr)
 
-    hr, count = tabStops.get_Count()
-    check_call("tabStops.get_Count", hr, count)
+        hr = paraFormat.put_Borders(borders)
+        self.check_call("paraFormat.put_Borders", hr)
 
-    hr = docRange.put_Start(end)
-    check_call("put_Start", hr)
+        hr, shading = para.get_Shading()
+        self.check_call("para.get_Shading", hr)
 
-    hr = docRange.put_End(end + 12)
-    check_call("put_End", hr)
+        hr = shading.put_BackgroundPatternColor(wpsapi.wdColorGreen)
+        self.check_call("shading.put_BackgroundPatternColor", hr)
 
-    hr, text = docRange.get_Text()
-    # should be "Hello, world"
-    check_call("get_Text", hr, text)
+        hr, tabStops = para.get_TabStops()
+        self.check_call("para.get_TabStops", hr)
 
-    # reset the range to find
-    hr, docRange = doc.get_Content()
+        hr, count = tabStops.get_Count()
+        self.check_call("tabStops.get_Count", hr)
 
-    hr, find = docRange.get_Find()
-    check_call("docRange.get_Find", hr, find)
+        # reset the range to find
+        hr, docRange = doc.get_Content()
 
-    hr, found = find.Execute("Hello, world", MatchWholeWord=True)
-    check_call("find.Execute", hr, found)
+        hr, find = docRange.get_Find()
+        self.check_call("docRange.get_Find", hr)
 
-    hr, font = docRange.get_Font()
-    check_call("docRange.get_Font", hr, font)
+        hr, found = find.Execute("Hello, world", MatchWholeWord=True)
+        self.check_call("find.Execute", hr)
+        self.assertTrue(found, "find.Execute")
 
-    hr, name = font.get_Name()
-    check_call("font.get_Name", hr, name)
+        hr, font = docRange.get_Font()
+        self.check_call("docRange.get_Font", hr)
 
-    hr = font.put_Bold(True)
-    check_call("font.put_Bold", hr)
+        hr, name = font.get_Name()
+        self.check_call("font.get_Name", hr)
 
-    hr, selection = wpsApp.get_Selection()
-    check_call("get_Selection", hr, selection)
+        hr = font.put_Bold(True)
+        self.check_call("font.put_Bold", hr)
 
-    hr = selection.SetRange(0, 10)
-    check_call("SetRange", hr)
+        hr, selection = wpsApp.get_Selection()
+        self.check_call("get_Selection", hr)
 
-    hr, start = selection.get_Start()
-    check_call("get_Start", hr, start)
+        hr = selection.SetRange(0, 10)
+        self.check_call("SetRange", hr)
 
-    hr, end = selection.get_End()
-    check_call("get_End", hr, end)
+        hr, start = selection.get_Start()
+        self.check_call("get_Start", hr)
 
-    hr, text = selection.get_Text()
-    check_call("get_Text", hr, text)
+        hr, end = selection.get_End()
+        self.check_call("get_End", hr)
 
-    hr, tables = doc.get_Tables()
-    check_call("doc.get_Tables", hr, tables)
+        hr, text = selection.get_Text()
+        self.check_call("get_Text", hr)
 
-    hr, pos = selection.EndKey(Unit=wpsapi.wdStory)
-    check_call("selection.EndKey", hr, pos)
+        hr, tables = doc.get_Tables()
+        self.check_call("doc.get_Tables", hr)
 
-    hr = selection.InsertParagraphAfter()
-    hr, pos = selection.EndKey(Unit=wpsapi.wdStory)
+        hr, pos = selection.EndKey(Unit=wpsapi.wdStory)
+        self.check_call("selection.EndKey", hr)
 
-    hr, selRange = selection.get_Range()
-    check_call("selection.get_Range", hr, selRange)
+        hr = selection.InsertParagraphAfter()
+        hr, pos = selection.EndKey(Unit=wpsapi.wdStory)
 
-    hr, table = tables.Add(selRange, 2, 2)
-    check_call("tables.Add", hr, table)
+        hr, selRange = selection.get_Range()
+        self.check_call("selection.get_Range", hr)
 
-    hr, cell = table.Cell(1, 2)
-    check_call("table.Cell", hr, cell)
+        hr, table = tables.Add(selRange, 2, 2)
+        self.check_call("tables.Add", hr)
 
-    hr, cellRange = cell.get_Range()
-    check_call("cell.get_Range", hr, cellRange)
+        hr, cell = table.Cell(1, 2)
+        self.check_call("table.Cell", hr)
 
-    hr = cellRange.put_Text("cell")
-    check_call("cellRange.put_Text", hr)
+        hr, cellRange = cell.get_Range()
+        self.check_call("cell.get_Range", hr)
 
-    hr, shapes = doc.get_Shapes()
-    check_call("doc.get_Shapes", hr, shapes)
+        hr = cellRange.put_Text("cell")
+        self.check_call("cellRange.put_Text", hr)
 
-    hr, shape = shapes.AddShape(wpsapi.msoShapeMoon, 0, 0, 100, 100)
-    check_call("shapes.AddShape", hr, shape)
+        hr, shapes = doc.get_Shapes()
+        self.check_call("doc.get_Shapes", hr)
 
-    hr = shape.put_AlternativeText("this is a moon shape")
-    check_call("shape.put_AlternativeText", hr)
+        hr, shape = shapes.AddShape(wpsapi.msoShapeMoon, 0, 0, 100, 100)
+        self.check_call("shapes.AddShape", hr)
 
-    hr, fields = doc.get_Fields()
-    check_call("doc.get_Fields",hr, fields)
+        hr = shape.put_AlternativeText("this is a moon shape")
+        self.check_call("shape.put_AlternativeText", hr)
 
-    hr, field = fields.Add(selRange, Type=wpsapi.wdFieldDate)
-    check_call("fields.Add", hr, field)
+        hr, fields = doc.get_Fields()
+        self.check_call("doc.get_Fields", hr)
 
-    hr, bookmarks = doc.get_Bookmarks()
-    check_call("doc.get_Bookmarks", hr, bookmarks)
+        hr, field = fields.Add(selRange, Type=wpsapi.wdFieldDate)
+        self.check_call("fields.Add", hr)
 
-    hr, bkmRange = doc.Range(0, 10)
-    check_call("doc.Range", hr, bkmRange)
+        hr, bookmarks = doc.get_Bookmarks()
+        self.check_call("doc.get_Bookmarks", hr)
 
-    hr, bookmark = bookmarks.Add("BookmarkName", bkmRange)
-    check_call("bookmarks.Add", hr, bookmark)
+        hr, bkmRange = doc.Range(0, 10)
+        self.check_call("doc.Range", hr)
 
-    hr, count = bookmarks.get_Count()
-    check_call("bookmarks.get_Count", hr, count)
+        hr, bookmark = bookmarks.Add("BookmarkName")
+        self.check_call("bookmarks.Add", hr)
 
-    hr, name = bookmark.get_Name()
-    check_call("bookmark.get_Name", hr, name)
+        hr, count = bookmarks.get_Count()
+        self.check_call("bookmarks.get_Count", hr)
 
-    hr, tocs = doc.get_TablesOfContents()
-    check_call("doc.get_TablesOfContents", hr, tocs)
+        hr, name = bookmark.get_Name()
+        self.check_call("bookmark.get_Name", hr)
 
-    hr, bkmRange = bookmark.get_Range()
-    check_call("bookmark.get_Range", hr, bkmRange)
+        hr, tocs = doc.get_TablesOfContents()
+        self.check_call("doc.get_TablesOfContents", hr)
 
-    hr, toc = tocs.Add(bkmRange)
-    check_call("tocs.Add", hr, toc)
+        hr, bkmRange = bookmark.get_Range()
+        self.check_call("bookmark.get_Range", hr)
 
-    hr = doc.SaveAs("test.docx", wpsapi.wdFormatDocument)
-    check_call("SaveAs", hr)
+        hr, toc = tocs.Add(bkmRange)
+        self.check_call("tocs.Add", hr)
 
-    hr = doc.Close()
-    check_call("Close", hr)
+        hr = doc.SaveAs("test.docx", wpsapi.wdFormatDocument)
+        self.check_call("SaveAs", hr)
 
-    hr, doc = docs.Open("test.docx", ReadOnly=True)
-    check_call("Open", hr, doc)
+        hr = doc.Close()
+        self.check_call("Close", hr)
 
-    hr, readOnly = doc.get_ReadOnly()
-    check_call("get_ReadOnly", hr, readOnly)
+        hr, doc = docs.Open("test.docx", ReadOnly=True)
+        self.check_call("Open", hr)
 
-    hr, window = doc.get_ActiveWindow()
-    check_call("doc.get_ActiveWindow", hr, window)
+        hr, readOnly = doc.get_ReadOnly()
+        self.check_call("get_ReadOnly", hr)
+        self.assertTrue(readOnly, "doc.get_ReadOnly")
 
-    hr, caption = window.get_Caption()
-    check_call("window.get_Caption", hr, caption)
+        hr, window = doc.get_ActiveWindow()
+        self.check_call("doc.get_ActiveWindow", hr)
 
-    doc.Close()
-    check_call("Close", hr)
+        hr, caption = window.get_Caption()
+        self.check_call("window.get_Caption", hr)
 
-    hr, doc = docs.Add(Visible=False)
-    check_call("Add", hr, doc)
+        doc.Close()
+        self.check_call("Close", hr)
 
-    hr, name = doc.get_Name()
-    check_call("get_Name", hr, name)
+        hr, doc = docs.Add(Visible=False)
+        self.check_call("Add", hr)
 
-    props = doc.CustomDocumentProperties
-    print("Doc Props:")
-    for prop in RpcIter(props):
-        print("\t %s: %s" % (prop.Name, prop.Value))
+        hr, name = doc.get_Name()
+        self.check_call("get_Name", hr)
 
-    props = doc.BuiltInDocumentProperties
-    print("Builtin Props:")
-    for prop in RpcIter(props):
-        print("\t %s: %s" % (prop.Name, prop.Value))
+        props = doc.CustomDocumentProperties
+        for prop in RpcIter(props):
+            self.assertIsNotNone(prop.Name)
 
-    hr, name = wpsApp.get_Name()
-    check_call("get_Name", hr, name)
+        props = doc.BuiltInDocumentProperties
+        for prop in RpcIter(props):
+            self.assertIsNotNone(prop.Name)
 
-    hr, version = wpsApp.get_Version()
-    check_call("get_Version", hr, version)
+        hr, name = wpsApp.get_Name()
+        self.check_call("get_Name", hr)
 
-    hr, build = wpsApp.get_Build()
-    check_call("get_Build", hr, build)
+        hr, version = wpsApp.get_Version()
+        self.check_call("get_Version", hr)
 
-    hr, caption = wpsApp.get_Caption()
-    check_call("get_Caption", hr, caption)
+        hr, build = wpsApp.get_Build()
+        self.check_call("get_Build", hr)
 
-    hr = wpsApp.put_Caption("test标题")
-    check_call("put_Caption", hr)
+        hr, caption = wpsApp.get_Caption()
+        self.check_call("get_Caption", hr)
 
-    hr, caption = wpsApp.get_Caption()
-    check_call("get_Caption", hr, caption)
+        hr = wpsApp.put_Caption("test标题")
+        self.check_call("put_Caption", hr)
 
-    hr = wpsApp.put_Visible(False)
-    check_call("put_Visible", hr)
+        hr, caption = wpsApp.get_Caption()
+        self.check_call("get_Caption", hr)
+        self.assertEqual(caption, "test标题")
 
-    hr, visible = wpsApp.get_Visible()
-    check_call("get_Visible", hr, visible)
+        hr = wpsApp.put_Visible(False)
+        self.check_call("put_Visible", hr)
 
-    hr = wpsApp.put_Visible(True)
-    check_call("put_Visible", hr)
+        hr, visible = wpsApp.get_Visible()
+        self.check_call("get_Visible", hr)
 
-    hr, visible = wpsApp.get_Visible()
-    check_call("get_Visible", hr, visible)
+        hr = wpsApp.put_Visible(True)
+        self.check_call("put_Visible", hr)
 
-    if not wpsApp.openDocument(os.path.dirname(os.path.realpath(__file__)) + "/../pyproject.toml", True):
-        print("Can't open document")
+        hr, visible = wpsApp.get_Visible()
+        self.check_call("get_Visible", hr)
+        self.assertTrue(visible, "wpsApp.get_Visible")
 
-    print(wpsApp.insertDocumentField("test"))
-    print(wpsApp.insertDocumentField("公文域"))
-    print(wpsApp.getAllDocumentField())
-    print(wpsApp.getForceBackUpEnabled())
+        if not wpsApp.openDocument(os.path.dirname(os.path.realpath(__file__)) + "/../pyproject.toml", True):
+            print("Can't open document")
 
-    hr = wpsApp.Quit(wpsapi.wdDoNotSaveChanges)
-    check_call("Quit", hr)
+        self.assertTrue(wpsApp.insertDocumentField("test"))
+        self.assertTrue(wpsApp.insertDocumentField("公文域"))
+        self.assertEqual(wpsApp.getAllDocumentField(), "test,公文域")
+        # wpsApp.getForceBackUpEnabled()
+
+        hr = wpsApp.Quit(wpsapi.wdDoNotSaveChanges)
+        self.check_call("Quit", hr)
 
 
 if __name__ == "__main__":
-    test()
+    unittest.main()
